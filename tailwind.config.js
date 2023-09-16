ac943db81d7f9ec517b0c2d45c638dd92526d1ea
Luke Shea <shealuke@yahoo.com>
2023-09-15 23:28:26 -0500

    Spent an hour just getting the font working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        colors: {
+            white: '#FFFFFF',
+            'background-black': '#070724',
+            'dark-grey': '#38384F',
+            'light-grey': '#838391',
+            'light-blue': '#419EBB',
+            'dark-yellow': '#EDA249',
+            purple: '#6F2ED6',
+            'dull-orange': '#D14C32',
+            red: '#D83A34',
+            'dirty-orange': '#CD5120',
+            'sea-green': '#1EC2A4',
+            'light-blue': '#2D68F0',
+        },
 
+        extend: {
+            fontFamily: {
+                antonio: ['Antonio'],
+                spartan: ['"League Spartan"'],
+            },
+        },
+    },
+    plugins: [require('daisyui')],
+};
